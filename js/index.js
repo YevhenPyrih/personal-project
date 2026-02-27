20018a7bc10446e0b04aed964796fd34afbbeb7f
YevhenPyrih <140730303+YevhenPyrih@users.noreply.github.com>
2026-02-27 02:38:33 +0000

    finally made the damn carousel

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+async function init() {
+    // Динамічний імпорт потребує відносного шляху з ./
+    await import('./index.certificates.js');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
